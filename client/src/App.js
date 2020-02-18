66690453daee7c4af118a45d9af9de1b553a5938
Sandra <sandrahh@stud.ntnu.no>
2020-02-18 10:47:28 +0100

    Add FrontPage to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FrontPage from "./components/frontpage/FrontPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Hello World
+        <FrontPage />
       </header>
     </div>
   );
