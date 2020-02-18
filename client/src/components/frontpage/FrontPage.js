ec97e8f6545b38b74af11395544bdec8eeadd03c
Sandra <sandrahh@stud.ntnu.no>
2020-02-18 10:43:13 +0100

    Add logo and button

--- client/src/components/frontpage/FrontPage.js
+++ client/src/components/frontpage/FrontPage.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
-import logo from "./../../logo.svg";
 import "./../../App.css";
+import logo from "./hybelvask.svg"
 
 class FrontPage extends Component {
+
   render() {
     return (
       <div className="FrontPage">
         <header className="FrontPage-header">
-          <img src={logo} className="FrontPage-logo" alt="logo" />
-          <h3 className="FrontPage-title">
+        <img src={logo} style={ { height: 200, width: 200 } } className="FrontPage-logo" alt="logo" />
+          <h1 className="FrontPage-title">
           Studentskipnaden i Fredrikstad
+          </h1>
+          <h3 className="FrontPage-title">
+          Hybelvask
           </h3>
         </header>
         <p className="Frontpage-intro">
-        Hybelvask
+        Logg inn
         </p>
+        <button className="Student-button">
+        Student
+        </button>
+        <button className="Admin-button">
+        Ansatt
+        </button>
       </div>
     );
   }
