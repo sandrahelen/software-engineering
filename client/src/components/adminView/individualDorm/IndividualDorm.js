f0bdf99cca6e57851ac3afcaf162f6ba490bdd79
Fredrikbjornland <fredrik.bjornland@vikenfiber.no>
2020-02-19 22:00:12 +0100

    se medlemmer i kollektiv

--- client/src/components/adminView/individualDorm/IndividualDorm.js
+++ client/src/components/adminView/individualDorm/IndividualDorm.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EditMembers from './editMembers/EditMembers';
 import './IndividualDorm.css';
 
 const IndividualDorm = ({ dorm }) => {
+    const [showEditMemebers, setShowEditMembers] = useState(false)
+
+
     const approve = () => {
         alert("Kollektivet er godkjent")
     }
     
     return (
-        <div className="dormBody">
+        <div className="dormBody" style={{borderStyle: 'solid'}}>
             <p>Romnummer: {dorm.kollektivnummer}</p>
-            <p>Medlemmer: {dorm.medlemmer.length}</p>
+            <p onClick={() => setShowEditMembers(true)}>Medlemmer: {dorm.medlemmer.length}</p>
             <button
                 onClick={() => approve()}
                 className="approveButton">
                 Godkjenn
             </button>
-            <EditMembers />
+            <EditMembers showEditMemebers={showEditMemebers} setShowEditMembers={setShowEditMembers} members={dorm.medlemmer} />
         </div>
     )
 }
--- client/src/components/adminView/individualDorm/editMembers/EditMembers.js
+++ client/src/components/adminView/individualDorm/editMembers/EditMembers.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import './EditMembers.css';
 
-const EditMembers = () => {
+const EditMembers = ({ showEditMemebers, setShowEditMembers, members }) => {
+    console.log(showEditMemebers)
     return (
-        <div>
-            edit Members
+        <div className={showEditMemebers ? 'editMembers__overlay' : ''}>
+            {showEditMemebers && (
+                <div className="editMembers__body">
+                    <div className="editMembers__headerWrapper">
+                        <p className="editMembers__header">Beboere</p>
+                        <span
+                            className="editMembers__cancel"
+                            onClick={() => {
+                                setShowEditMembers(false);
+                            }}
+                            role="button"
+                        >
+                            X
+                    </span>
+                    </div>
+                    <div className="editMembers__list">
+                        {members.map((member, index) =>
+                            <div key={index} >{member}<FaTrashAlt /></div>
+                        )}
+                    </div>
+                    <button 
+                        className="editMembers__addMember"
+                        onClick={() => console.log("legg til")}
+                    >
+                        Legg til beboer
+                    </button>
+                </div>)
+            }
         </div>
     )
 }
