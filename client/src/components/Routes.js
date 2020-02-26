208a51aadd6245cabde744feefd72764ca68f71e
Oline Zachariassen <olinezachariassen@dhcp-10-22-46-160.wlan.ntnu.no>
2020-02-26 16:05:12 +0100

    login

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -4,10 +4,12 @@ import AdminView from "./adminView/AdminView";
 import CampusView from "./campusView/CampusView";
 import FrontPage from "./FrontPage/FrontPage";
 import Login from "./login/Login";
+import Login2 from "./login2/Login2";
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={FrontPage} />
     <Route exact path="/LoginPage" component={Login} />
+    <Route exact path="/LoginPage2" component={Login2} />
     <Route exact path="/AdminView" component={AdminView} />
     <Route exact path="/CampusView" component={CampusView} />
   </Switch>
