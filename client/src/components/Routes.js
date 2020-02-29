b2921bb25dbfb06203168e01b0fb0eb9c8bfebce
edvardds <edvardds@stud.ntnu.no>
2020-02-29 18:24:59 +0100

    vet ikke

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -4,13 +4,10 @@ import AdminView from "./adminView/AdminView";
 import CampusView from "./campusView/CampusView";
 import FrontPage from "./frontPage/FrontPage";
 import Login from "./login/Login";
-<<<<<<< HEAD
 import StudentView from "./studentView/StudentView";
 
 
-=======
 import Login2 from "./login2/Login2";
->>>>>>> 208a51aadd6245cabde744feefd72764ca68f71e
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={FrontPage} />
