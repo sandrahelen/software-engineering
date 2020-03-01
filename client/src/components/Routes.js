385a45c076c5190a556d473c0a6fcc4db7898a2a
edvardds <edvardds@stud.ntnu.no>
2020-03-01 11:58:24 +0100

    added user login

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import AdminView from "./adminView/AdminView";
 import CampusView from "./campusView/CampusView";
-import FrontPage from "./frontPage/FrontPage";
+import FrontPage from "./FrontPage/FrontPage";
 import Login from "./login/Login";
 import StudentView from "./studentView/StudentView";
 
