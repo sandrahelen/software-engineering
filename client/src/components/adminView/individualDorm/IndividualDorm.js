819a585bce416905f9bf51e4459eb7628f9a9b10
Fredrikbjornland <fredrik.bjornland@vikenfiber.no>
2020-03-10 16:45:55 +0100

    added approve dorm button

--- client/src/components/adminView/individualDorm/IndividualDorm.js
+++ client/src/components/adminView/individualDorm/IndividualDorm.js
@@ -4,14 +4,17 @@ import EditMembers from './editMembers/EditMembers';
 import { useUser } from '../../../hooks/user';
 import { useCleaningList } from '../../../hooks/vaskeliste';
 import { FaTrashAlt } from 'react-icons/fa';
+import { FaRegCheckCircle } from 'react-icons/fa';
+import { FaRegWindowClose } from 'react-icons/fa';
 import './IndividualDorm.css';
 
 const IndividualDorm = ({ dorm, render, setRender }) => {
     const [showEditMemebers, setShowEditMembers] = useState(false);
     const { cleaningList } = useCleaningList(dorm.vaskeliste);
     const { users, setUsers } = useUser(dorm._id);
-
+    console.log(dorm)
     useEffect(() => {
+        console.log("test")
         axios.get('http://localhost:5000/api/user')
             .then(response => {
                 if (response.data) {
@@ -26,7 +29,19 @@ const IndividualDorm = ({ dorm, render, setRender }) => {
     }, [dorm._id, render]);
 
     const approve = () => {
-        alert("Kollektivet er godkjent")
+        axios.put(`http://localhost:5000/api/kollektiv/${dorm._id}`,
+        {
+            "godkjentVask": !dorm.godkjentVask,
+        }
+        )
+        .then(response => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        setRender(!render);
+        alert(`Kollektivet er ${!dorm.godkjentVask? "godkjent" : "underkjent"}`)
     }
     const deleteDorm = (id) => {
         axios.delete(`http://localhost:5000/api/kollektiv/${id}`)
@@ -40,13 +55,27 @@ const IndividualDorm = ({ dorm, render, setRender }) => {
     };
     return (
         <div className="dormBody">
+            {dorm.godkjentVask ? 
+                <FaRegCheckCircle className="check"/>
+                :
+                <FaRegWindowClose className="cross"/>
+            }
             <p>Romnummer: {dorm.kollektivnummer}</p>
             <p onClick={() => setShowEditMembers(true)} className="individualDorm__members">Medlemmer: {users.length}</p>
-            <button
-                onClick={() => approve()}
-                className="approveButton">
-                Godkjenn
-            </button>
+            {dorm.godkjentVask ?
+
+                <button
+                    onClick={() => approve()}
+                    className="approveButton">
+                    Underkjenn
+                </button>
+                :
+                <button
+                    onClick={() => approve()}
+                    className="approveButton">
+                    Godkjenn
+                </button>
+            }
             <EditMembers showEditMemebers={showEditMemebers} setShowEditMembers={setShowEditMembers} members={users} setMembers={setUsers} dormId={dorm._id} render={render} setRender={setRender} />
             <FaTrashAlt
                 className="individualDorm__delete"
