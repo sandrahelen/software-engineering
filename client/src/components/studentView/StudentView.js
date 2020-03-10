11e2bf48be7cc649e7881aa3ba895fb7cf3ca1ae
Sandra <sandrahh@stud.ntnu.no>
2020-03-10 15:58:06 +0100

    Dummy kollektiv og vaskeliste

--- client/src/components/studentView/StudentView.js
+++ client/src/components/studentView/StudentView.js
@@ -1,61 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { useDormWithDormId } from "../../hooks/dormForStudentID"
-import { useCampusForStudentID } from "../../hooks/campusIdForUser"
-import { useCleaningList } from "../../hooks/vaskeliste"
-import { Link } from 'react-router-dom';
-import queryString from 'query-string';
-import axios from 'axios';
-import './studentView.css'
+import React, { useState, useEffect } from "react";
+import { useDormWithDormId } from "../../hooks/dormForStudentID";
+import { useCampusForStudentID } from "../../hooks/campusIdForUser";
+import { useCleaningList } from "../../hooks/vaskeliste";
+import { Link } from "react-router-dom";
+/*import SplitPane, { Pane } from 'react-split-pane';*/
+import queryString from "query-string";
+import axios from "axios";
+import "./studentView.css";
 
 const StudentView = ({ location }) => {
-    const { username } = queryString.parse(location.search);
-    const [user, setUser] = useState({
-        etternavn: "",
-        fornavn: "",
-        kollektiv: "",
-        password: "",
-        username: "",
-        __v: 0,
-        _id: "",
-    });
+  const { username } = queryString.parse(location.search);
+  const [user, setUser] = useState({
+    etternavn: "",
+    fornavn: "",
+    kollektiv: "",
+    password: "",
+    username: "",
+    __v: 0,
+    _id: ""
+  });
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/user')
-            .then(response => {
-                if (response.data) {
-                    let filteredUser = response.data.find(
-                        user => user.username === username)
-                    setUser(filteredUser)
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, [location])
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/user")
+      .then(response => {
+        if (response.data) {
+          let filteredUser = response.data.find(
+            user => user.username === username
+          );
+          setUser(filteredUser);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [location]);
 
-    const dorm = useDormWithDormId(user.kollektiv);
-    const vaskelisteId = useCampusForStudentID(dorm.vaskeliste);
-    const { cleaningList } = useCleaningList(vaskelisteId);
-    return (
-        <div className="studentView">
-            <h1 className="studentView__header">Kollektiv Nr {dorm.kollektivnummer}</h1>
-            <div className="studentView__body">
-                <p className="studentView__header2">Vaskeliste</p>
-                <div className="studentView__cleaningList">
-                    {cleaningList.liste.map((listItem, index) =>
-                        <div
-                            className="studentView__listItem"
-                            key={index}>{listItem}</div>
-                    )}
-                </div>
+  const students = ["Kari", "Ole", "Trine", "Emma"];
+  const dorm = useDormWithDormId(user.kollektiv);
+  const vaskelisteId = useCampusForStudentID(dorm.vaskeliste);
+  const { cleaningList } = useCleaningList(vaskelisteId);
+  return (
+    <div className="studentView">
+      <h1 className="studentView__header">
+        Kollektiv Nr {dorm.kollektivnummer}
+      </h1>
+      <div className="studentView__body">
+				<div className="studentView__studentList">
+          <p className="studentView__header3">Kollektivmedlemmer</p>
+          {students.map((listStudent, index) => (
+            <div className="studentView__students" key={index}>
+              {listStudent}
             </div>
-            <div className="studentView__buttonWrapper">
-                <Link to="/">
-                    <button className="studentView__button">Log ut</button>
-                </Link>
-            </div>
+          ))}
         </div>
-    )
-}
+			<div className="studentView__space"></div>
+				<div className="studentView__cleaningList">
+          <p className="studentView__header2">Vaskeliste</p>
+          {cleaningList.liste.map((listItem, index) => (
+            <div className="studentView__listItem" key={index}>
+              {listItem}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="studentView__buttonWrapper">
+        <Link to="/">
+          <button className="studentView__button">Log ut</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default StudentView;
