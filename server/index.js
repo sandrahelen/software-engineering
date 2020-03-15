e69c75faddf9229141f583c5e06de4567496cc98
Fredrikbjornland <fredrik.bjornland@vikenfiber.no>
2020-03-15 15:00:49 +0100

    setting up to push to heroku

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 
 //TODO Add mongoose link in .env file
 //Kobler opp mot MongoDB databasen
-mongoose.connect('mongodb+srv://fredrik:12345@pu48-e21uc.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://fredrik:12345@pu48-e21uc.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`Database connected successfully`))
     .catch(err => console.log(err));
 
@@ -50,3 +50,5 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
+
+module.exports = app;
