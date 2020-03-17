ba4bcc819cb4383eb88e97c76efd2d75669fb6ff
Oline Zachariassen <olinezachariassen@Olines-MacBook-Pro.local>
2020-03-17 12:45:27 +0100

    style studentview

--- client/src/components/campusView/CampusView.js
+++ client/src/components/campusView/CampusView.js
@@ -1,52 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { useCampuses } from '../../hooks/studentby';
-import './CampusView.css'
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useCampuses } from "../../hooks/studentby";
+import "./CampusView.css";
 
 const CampusView = ({ location }) => {
-    const [admin, setAdmin] = useState({
-        vaskeliste: "",
-        navn: "",
-        admin: "",
-        _id: ""
-    });
+  const [admin, setAdmin] = useState({
+    vaskeliste: "",
+    navn: "",
+    admin: "",
+    _id: ""
+  });
 
-    const { username } = queryString.parse(location.search);
+  const { username } = queryString.parse(location.search);
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/admin')
-            .then(response => {
-                if (response.data) {
-                    let filteredadmin = response.data.find(
-                        admin => admin.username === username)
-                    setAdmin(filteredadmin);
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, [location]);
-    
-    const { campuses } = useCampuses(admin._id);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/admin")
+      .then(response => {
+        if (response.data) {
+          let filteredadmin = response.data.find(
+            admin => admin.username === username
+          );
+          setAdmin(filteredadmin);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [location]);
 
-    return (
-        <div className="campusView__body">
-            <div className="campusView__header">
-                <h1>Mine studentbyer</h1>
-            </div>
-            <div className="campusView-campuses">
-                {campuses.map((campus, index) =>
-                    <Link key={index} className="campusView-text" to={`/AdminView?campusId=${campus._id}&campusName=${campus.navn}&cleaningListId=${campus.vaskeliste}`}>
-                        <p>{campus.navn}</p>
-                    </Link>
+  const { campuses } = useCampuses(admin._id);
 
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="campusView__body">
+      <div className="campusView__header">
+        <h1>Mine studentbyer</h1>
+      </div>
+      <div className="campusView-campuses">
+        {campuses.map((campus, index) => (
+          <Link
+            key={index}
+            style={{ height: 300, width: 300 }}
+            className="campusView-text"
+            to={`/AdminView?campusId=${campus._id}&campusName=${campus.navn}&cleaningListId=${campus.vaskeliste}`}
+          >
+            <p>{campus.navn}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CampusView;
-
