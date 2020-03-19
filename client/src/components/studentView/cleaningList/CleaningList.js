940cf5ea7b09e725a07a06e5f2a42bbb4d93ced9
Fredrikbjornland <fredrik.bjornland@vikenfiber.no>
2020-03-19 20:07:35 +0100

    rettet opp en import

--- client/src/components/studentView/cleaningList/CleaningList.js
+++ client/src/components/studentView/cleaningList/CleaningList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import CheckBox from "../checkBox/CheckBox";
+import CheckBox from "./checkBox/CheckBox";
 import axios from "axios";
 import "./cleaningList.css";
 
