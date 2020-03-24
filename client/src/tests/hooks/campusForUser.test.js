5a528363c53da91bde26f859b7571ad8b9124337
edvardds <edvardds@stud.ntnu.no>
2020-03-24 12:10:11 +0100

    added some more tests

---
+++ client/src/tests/hooks/campusForUser.test.js
@@ -0,0 +1,39 @@
+import { unmountComponentAtNode, render } from "react-dom";
+import React, { Component } from "react";
+import { act } from "react-dom/test-utils";
+import {index,  useCampusForStudentID} from "../../hooks/campusIdForUser/index";
+import { renderHook} from '@testing-library/react-hooks'
+import axios from "axios";
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+jest.mock("axios");
+
+it("Basic test for correct rendering", () => {
+
+    const resp = {
+        "_id": "5e5bfced94e2442eaf559c3f",
+        "vaskeliste": "5e552a301c9d440000a0d76a",
+        "navn": "Lerkendal Studentby",
+        "admin": "5e5bf1b6ff72ba1ff4383bc1",
+        "__v": 0
+    }
+    axios.get.mockImplementation(() => Promise.resolve(resp));
+    const { result } = renderHook(() =>  useCampusForStudentID("5e5bfced94e2442eaf559c3f"));
+    expect(result).toBeDefined();
+});
+
+
+
